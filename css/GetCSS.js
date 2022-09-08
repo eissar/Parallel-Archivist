abb98e10523027b64c27b60e7288e0dc984548ce
eissar <61753097+eissar@users.noreply.github.com>
2022-09-07 23:12:33 -0400

    Update and rename GetCSS to GetCSS.js

--- css/GetCSS.js
+++ css/GetCSS.js
@@ -1,7 +1,7 @@
 /* Using library from https://github.com/jhildenbiddle/get-css-data*/
-function getCSS{
+function getCSS(element){
 getCssData({
-  rootElement: selectedElement
+  rootElement: element
   onComplete: function (cssText, cssArray, nodeArray) {
     //console.log (cssText); // 1
     //console.log (cssArray); // 2
