4d9cb45f594a97c595d52bb22503c85191c294e4
eissar <61753097+eissar@users.noreply.github.com>
2022-09-07 23:40:38 -0400

    Create exitFunction.js
    I don't know if this is needed

---
+++ exitFunction.js
@@ -0,0 +1,12 @@
+fucntion exitFunction() {
+console.groupCollapsed("Parallel Archivist - Exiting selection mode...")
+  
+//not implemented//
+window.removeEventListener("click", getElements); //check if there are any event listeners left 
+
+//maybe working//
+if (document.contains(document.getElementById('hoverbox')) {
+    console.log("hoverbox destroyed")}//check if hoverbox was destroyed//
+
+console.groupEnd(); 
+}
